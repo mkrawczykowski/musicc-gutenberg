a39efd08ff1c14080c9092931dd76484453ce448
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-06-20 14:38:59 +0200

    track's HTML

--- index.php
+++ index.php
@@ -163,6 +163,43 @@
 			<div class="container">
 				<div class="row">
 					<div class="col">
+						<div class="track">
+							<h3 class="track-name">Please, don't stop the music</h3>
+							<p class="track-description">Tutaj będzie krótki opis kawałka, który nie zawsze będzie występował. Dam tutaj ograniczony boksik, czyli mały boks.</p>
+							<div class="track-box">
+								<div class="track-box__player">
+									<button class="track-box__button"></button>
+									<div class="track-box__track">
+										<span class="track-box__pointer"></span>
+									</div>
+									<span>05:50</span>
+								</div>
+								<ul class="track-box__icons">
+									<li class="track-box__icon track-box__icon--download"></li>
+									<li class="track-box__icon track-box__icon--share"></li>
+								</ul>
+							</div>
+							<ul>
+								<li>happy</li>
+								<li>mysterious</li>
+								<li>happy</li>
+								<li>warm</li>
+								<li>happy</li>
+								<li>mysterious</li>
+								<li>happy</li>
+								<li>piano</li>
+								<li>warm</li>
+								<li>mysterious</li>
+								<li>piano</li>
+								<li>happy</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- <div class="container">
+				<div class="row">
+					<div class="col">
 						<div class="heading-text">
 							<h1 class="heading-text__heading">Free CC music</h1>
 							<p class="heading-text__text">produced by professional web developer<br>
@@ -170,7 +207,7 @@
 						</div>
 					</div>
 				</div>
-			</div>
+			</div> -->
 	</div>
 		
 	</main>
