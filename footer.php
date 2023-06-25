890f0e2e6323a1a93700cf1b3daaf1a2093a01f2
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-06-25 10:49:22 +0200

    test of cookies

--- footer.php
+++ footer.php
@@ -14,6 +14,30 @@
 </footer>
 
   <?php wp_footer(); ?>
+  <script>
+    function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      }
+
+      const getCookie = (name) => {
+        return document.cookie.split('; ').reduce((r, v) => {
+          const parts = v.split('=')
+          return parts[0] === name ? decodeURIComponent(parts[1]) : r
+        }, '')
+      }
+
+      // setCookie('cookiename', 'yes', 100000000000);
+      console.log(getCookie('cookiename'));
+      if (getCookie('cookiename') === ''){
+        console.log('no cookie')
+      }
+      if (getCookie('cookiename') === 'yes'){
+        console.log('cookie with yes')
+      }
+  </script>
 
 </body>
 </html>
