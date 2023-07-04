88803c74d08b8442454df9404148d6484a183cbc
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-07-04 11:51:05 +0200

    hamburger working & some header styling

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,15 +1,15 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   const hamburger = document.getElementById('js-hamburger');
-//   const mainNav = document.getElementById('js-main-nav');
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.getElementById('js-hamburger');
+  const mainNav = document.getElementById('js-main-nav');
 
-//   hamburger.addEventListener('click', () => {
-//     hamburger.classList.toggle('active');
-//     mainNav.classList.toggle('active');
-//     if (hamburger.classList.contains('active')) {
-//       document.body.style.overflow = 'hidden';
-//     }
-//     if (!hamburger.classList.contains('active')) {
-//       document.body.style.removeProperty('overflow');
-//     }
-//   });
-// });
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mainNav.classList.toggle('active');
+    if (hamburger.classList.contains('active')) {
+      document.body.style.overflow = 'hidden';
+    }
+    if (!hamburger.classList.contains('active')) {
+      document.body.style.removeProperty('overflow');
+    }
+  });
+});
