6a1c81b8a3292a987fd87ce0ec49cce08a4a8daa
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-07-11 15:45:40 +0200

    better cursor on hover in mega menu

--- src/js/main-menu.js
+++ src/js/main-menu.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const mainMenuWithChildrenItems = document.querySelectorAll('.main-menu__menu-item.has-children');
+  const mainMenuWithChildrenItems = document.querySelectorAll('.main-menu__menu-item.has-children .main-menu__menu-link');
   console.log(mainMenuWithChildrenItems);
   mainMenuWithChildrenItems.forEach(mainMenuWithChildrenItem => {
+    thisParent = mainMenuWithChildrenItem.closest('.main-menu__menu-item');
     mainMenuWithChildrenItem.addEventListener('click', () => {
-      mainMenuWithChildrenItem.classList.toggle('active');
+      thisParent.classList.toggle('active');
     })
   })
 });
