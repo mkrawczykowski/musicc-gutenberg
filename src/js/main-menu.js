447ac06faad8fba2341cc2c3a882a86bc6e75187
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-07-12 11:12:11 +0200

    mobile menu problem fixed

--- src/js/main-menu.js
+++ src/js/main-menu.js
@@ -2,17 +2,30 @@ document.addEventListener('DOMContentLoaded', function () {
   const mainMenuWithChildrenItems = document.querySelectorAll('.main-menu__menu-item.has-children .main-menu__menu-link');
   console.log(mainMenuWithChildrenItems);
   mainMenuWithChildrenItems.forEach(mainMenuWithChildrenItem => {
-    thisParent = mainMenuWithChildrenItem.closest('.main-menu__menu-item');
-    mainMenuWithChildrenItem.addEventListener('click', () => {
+
+    const thisParent = mainMenuWithChildrenItem.closest('.main-menu__menu-item');
+    mainMenuWithChildrenItem.addEventListener('click', (event) => {
+      console.log('mainMenuWithChildrenItem');
+      console.log(mainMenuWithChildrenItem);
+      console.log('click above')
+      console.log('thisParent');
+      console.log(thisParent);
       thisParent.classList.toggle('active');
+
+      console.log('event')
+      console.log(event.target)
+      console.log('===============================');
     })
   });
 
   const mainMenuWidgetListNames = document.querySelectorAll('.js-main-menu-widget-list-name');
   mainMenuWidgetListNames.forEach(mainMenuWidgetListName => {
-    mainMenuWidgetListName.addEventListener('click', () => {
-      thisParent = mainMenuWidgetListName.closest('.main-menu__widget');
+    mainMenuWidgetListName.addEventListener('click', (event) => {
+      console.log('click below')
+      const thisParent = mainMenuWidgetListName.closest('.main-menu__widget');
       thisParent.classList.toggle('active');
+      console.log(event.target);
+      console.log('===============================');
     })
   })
 });
